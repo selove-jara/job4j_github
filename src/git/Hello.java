5bea7d9705607fcebcaabd4d6f1039da280a35e7
selov <selove.jara@gmail.com>
2024-08-22 18:26:15 +0400

    3.1. Добавить изменения в последний коммит. [#504829 #510148]"

--- src/git/Hello.java
+++ src/git/Hello.java
@@ -2,6 +2,6 @@ package git;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("We well not use 'Hello world!'");
     }
 }
