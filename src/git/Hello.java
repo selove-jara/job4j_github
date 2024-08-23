cf7adf9fabcad5143134db957df193771e34915c
selov <selove.jara@gmail.com>
2024-08-23 17:27:34 +0400

    3.4. Временное хранение. git stash. [#504832]
    3.4. Временное хранение. git stash. [#504832]

--- src/git/Hello.java
+++ src/git/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("We well not use 'Hello world!'");
         System.out.println("Nothing of the kind");
         System.out.println("Hello world!");
+        System.out.println("How are you?");
     }
 }
