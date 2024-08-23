2bc687fc8e20e6ad6829ba6f80bbbfafdbd19987
selov <selove.jara@gmail.com>
2024-08-23 12:18:40 +0400

    3.2. Откат изменений[#504830]

--- src/git/Hello.java
+++ src/git/Hello.java
@@ -3,5 +3,7 @@ package git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("We well not use 'Hello world!'");
+        System.out.println("Nothing of the kind");
+        System.out.println("Hello world!");
     }
 }
